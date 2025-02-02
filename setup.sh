ce6a21a1442ac1756f3688d118d8a92175be76f4
Daniel Webb <danwebb94@gmail.com>
2025-02-02 23:25:24 +0000

    wip: setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+## Default terminal used by hyprland
+sudo pacman -Sy ghostty -y
+
+## Install Flatpack
+
+## Install browser of choice
+
+
+## Install oh my bash for a more feature rich bash setup
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
