cbde1081709ca5c2e95a7714953815c4e16b74d8
Daniel Webb <danwebb94@gmail.com>
2025-02-03 23:13:45 +0000

    basic ghostty config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
+## Install DE
+sudo pacman -S hyprland -y
+## Install File Explorer
+sudo pacman -Sy dolphin -y
 ## Default terminal used by hyprland
 sudo pacman -Sy ghostty -y
 
 ## Install Flatpack
+sudo pacman -S flatpak -y
 
 ## Install browser of choice
-
+pacman -S firefox -y
 
 ## Install oh my bash for a more feature rich bash setup
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
